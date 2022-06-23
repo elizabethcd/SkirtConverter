08efea3e02282cb8fb95f4e445c7019fd6c840f5
elizabethcd <55162529+elizabethcd@users.noreply.github.com>
2022-06-23 15:10:36 -0400

    Adding some more animation frames

--- skirt_converter.py
+++ skirt_converter.py
@@ -6,14 +6,30 @@ skirtsFoldername = "skirts"
 def main():
 	folderPath = Path(skirtsFoldername)
 
-	# Some hardcoded indices
+	# Hardcoded animation indices
 	walking = [["0@200", "1@200", "0@200", "2@200"],
 			   ["6@200", "7@200", "6@200", "8@200"],
 			   ["12@200", "13@200", "12@200", "14@200"]]
 
 	running = [["0@90", "1@60", "18@120", "1@60", "0@90", "2@60", "19@120", "2@60"],
 			   ["6@90", "21@140", "17@100", "6@90", "20@140", "11@100"],
-			   ["12@90", "13@60", "22@120", "13@60", "12@90", "14@60", "23@120", "14@6"]]
+			   ["12@90", "13@60", "22@120", "13@60", "12@90", "14@60", "23@120", "14@60"]]
+
+	heavyTool = [["66@150", "67@40", "68@40", "69@170", "70@75"],
+				 ["48@100", "49@40", "50@40", "51@220", "52@75"],
+				 ["36@100", "37@40", "38@40", "63@220", "62@75"]]
+
+	heavyToolPressed =[["66@600", "74@600", "75"],
+					   ["48@600", "72@600", "73"],
+					   ["36@600", "76@600", "77"]]
+
+	meleeWeapon = [["24@55", "25@45", "26@25", "27@25", "28@25", "29"],
+				   ["30@55", "31@45", "32@25", "33@25", "34@25", "35"],
+				   ["36@55", "37@45", "38@25", "39@25", "40@25", "41"]]
+
+	watering = [["54@75", "55@100", "25@500"],
+				["58@75", "59@100", "45@500"],
+				["62@75", "63@100", "46@500"]]
 
 	harvesting = [["54@100", "55@100", "56@100", "57@100"],
 				  ["58@100", "59@100", "60@100", "61@10"],
@@ -22,6 +38,13 @@ def main():
 	eating = [["84@250", "85@400", "86@401", "87@250", "88@250", "87@250", "88@250", "87@250", "0@250"]]
 
 	drinking = [["90@250", "91@150", "92@250", "93@200", "92@250", "93@200", "92@250", "93@200", "91@250", "90@50"]]
+
+	# Hardcoded static indices
+	horse = [["107","106","113"]]
+
+	sitting = [["107","117", "113"]]
+
+	standing = [["0","6","12"]]
 
 	# Get all images in skirts folder
 	filelist= [file for file in folderPath.glob('**/*') if file.is_file()]
@@ -32,15 +55,33 @@ def main():
 		finalImage = Image.new(mode="RGBA",size=(12*16,12000))
 
 		# Save all of the walking sprites row by row
-		saveAllSpriteRows(0, walking, originalImage, finalImage)
+		nextRowNum = saveAllSpriteRows(0, walking, originalImage, finalImage)
 
 		# Save all of the running sprites row by row
-		saveAllSpriteRows(3, running, originalImage, finalImage)
+		nextRowNum = saveAllSpriteRows(nextRowNum, running, originalImage, finalImage)
+
+		# Save all of the heavy tool sprites row by row
+		nextRowNum = saveAllSpriteRows(nextRowNum, heavyTool, originalImage, finalImage)
+
+		# Save all of the heavy tool sprites row by row
+		nextRowNum = saveAllSpriteRows(nextRowNum, heavyToolPressed, originalImage, finalImage)
+
+		# Save all of the heavy tool sprites row by row
+		nextRowNum = saveAllSpriteRows(nextRowNum, meleeWeapon, originalImage, finalImage)
+
+		# Save all of the heavy tool sprites row by row
+		nextRowNum = saveAllSpriteRows(nextRowNum, watering, originalImage, finalImage)
 
 		# Save all of the harvesting sprites row by row
-		saveAllSpriteRows(6, harvesting, originalImage, finalImage)
+		nextRowNum = saveAllSpriteRows(nextRowNum, harvesting, originalImage, finalImage)
 
-		finalImage = finalImage.crop((0,0,12*16,20*32))
+		# Save all of the eating sprites
+		nextRowNum = saveAllSpriteRows(nextRowNum, eating, originalImage, finalImage)
+
+		# Save all of the drinking sprites
+		nextRowNum = saveAllSpriteRows(nextRowNum, drinking, originalImage, finalImage)
+
+		finalImage = finalImage.crop((0,0,12*16,nextRowNum*32))
 
 		finalImage.save("finalImage.png")
 
@@ -58,6 +99,7 @@ def saveAllSpriteRows(initialRowNum, listName, originalImage, finalImage):
 			finalImage.paste(frameImage,(colNum*16,rowNum*32))
 			colNum = colNum + 1
 		rowNum = rowNum + 1
+	return rowNum
 
 # Call the main function
 main()
